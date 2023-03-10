bb88db7bc47195486f6989eff797bb3a79fc8840
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-03-10 09:01:03 +0700

    create banner component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+/* import components */
 import Head from "next/head";
 
+import Banner from "@/components/banner/banner";
+
+/* import styles */
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -9,6 +13,11 @@ export default function Home() {
         <title>Movstream</title>
       </Head>
       <h1>HI IM HOME PAGE</h1>
+      <Banner
+        title="Spiderman far from home"
+        subTitle="The ultimate adventure of spider-verse"
+        imgUrl="/static/spiderman-far-from-home-poster-landscape.webp"
+      />
     </>
   );
 }
