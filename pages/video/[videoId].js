1e4a4f391d7b90ddf4418a5f0f83c134583be9d9
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-03-11 15:28:10 +0700

    styling modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,5 +1,6 @@
 /* import functions */
 import { useRouter } from "next/router";
+import clsx from "classnames";
 
 /* import components */
 import Modal from "react-modal";
@@ -11,11 +12,20 @@ Modal.setAppElement("#__next");
 
 const Video = () => {
   const router = useRouter();
-  console.log({ router });
+
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description:
+      "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div>
-      video page {router.query.videoId}
       <Modal
         className={styles.modal}
         isOpen={true}
@@ -23,7 +33,37 @@ const Video = () => {
         onRequestClose={() => router.back()}
         overlayClassName={styles.overlay}
       >
-        <div>Modal body</div>
+        {
+          // embed youtube video
+          <iframe
+            className={styles.videoPlayer}
+            id="ytplayer"
+            type="text/html"
+            width="100%"
+            height="360"
+            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+            frameBorder="0"
+          ></iframe>
+        }
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
