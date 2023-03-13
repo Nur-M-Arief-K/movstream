4cf4c11405181fdf85066e9219d98b24084995e1
Nur-M-Arief-K <ariefnur141@gmail.com>
2023-03-13 12:11:10 +0700

    create stats API

---
+++ pages/api/stats.js
@@ -0,0 +1,26 @@
+/* import functions */
+import jwt from "jsonwebtoken";
+import { findVideoIdByUser } from "@/lib/db/hasura";
+
+export default async function stats(req, resp) {
+  if (req.method === "POST") {
+    try {
+      /* cookies are sent by default with every http request */
+      const token = req.cookies.token;
+      if (!token) {
+        resp.status(403).send({});
+      } else {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const videoId = req.query.videoId;
+        const userId = decodedToken.issuer;
+
+        const findVideoId = await findVideoIdByUser(token, userId, videoId);
+        console.log({ findVideoId });
+        resp.send({ msg: "it works", decodedToken, findVideoId });
+      }
+    } catch (error) {
+      console.error("Error occurred /stats", error);
+      resp.status(500).send({ done: false, error: error?.message });
+    }
+  }
+}
